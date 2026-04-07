8c14501b29f3f70c764b89b56c97bcd4fec69355
vmelniki2828 <76560283+vmelniki2828@users.noreply.github.com>
2026-04-07 22:11:05 +0300

    asd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,21 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      
+      <script>
+  (function(d,t) {
+    var BASE_URL="http://159.65.126.33:3000";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+      window.chatwootSDK.run({
+        websiteToken: 'DcDn84YZWMBt99kzCH4vT1wJ',
+        baseUrl: BASE_URL
+      })
+    }
+  })(document,"script");
+</script>
     </div>
   );
 };
