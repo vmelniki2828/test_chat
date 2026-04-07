9b783b19e708c07b170294e285729155ffe7408e
vmelniki2828 <76560283+vmelniki2828@users.noreply.github.com>
2026-04-07 23:44:35 +0300

    asd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,4 @@
-import { useEffect } from 'react';
-
-const BASE_URL = 'http://159.65.126.33:3000';
-const SDK_SRC = `${BASE_URL}/packs/js/sdk.js`;
-
 export const App = () => {
-  useEffect(() => {
-    if (document.querySelector(`script[src="${SDK_SRC}"]`)) return;
-
-    const script = document.createElement('script');
-    script.src = SDK_SRC;
-    script.async = true;
-    script.onload = () => {
-      window.chatwootSDK.run({
-        websiteToken: 'sVF7PMYGJzzzWBtbBxPZYu9v',
-        baseUrl: BASE_URL
-      });
-    };
-    document.head.appendChild(script);
-  }, []);
-
   return (
     <div
       style={{
@@ -29,6 +9,8 @@ export const App = () => {
         fontSize: 40,
         color: '#010101'
       }}
-    />
+    >
+
+    </div>
   );
 };
